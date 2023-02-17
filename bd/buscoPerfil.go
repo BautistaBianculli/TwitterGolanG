0c95c0da76bf7659e45772edcb4d57dd0434d354
Bautista Bianculli <bbianculli2000@gmail.com>
2023-02-16 21:28:32 -0300

    Busco Perfil

--- bd/buscoPerfil.go
+++ bd/buscoPerfil.go
@@ -8,15 +8,14 @@ import (
 	"github.com/BautistaBianculli/TwitterGolanG/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-
 )
 
 /*BuscoPerfil busca un perfil en la BD*/
-func BuscoPerfil(ID string)(models.User, error){
-	context, cancel := context.WithTimeout(context.Background(),time.Second*15)
+func BuscoPerfil(ID string) (models.User, error) {
+	context, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
 
-	db:= MongoCN.Database("Twitter")
+	db := MongoCN.Database("Twitter")
 	col := db.Collection("users")
 
 	var perfil models.User
@@ -24,14 +23,14 @@ func BuscoPerfil(ID string)(models.User, error){
 	objID, _ := primitive.ObjectIDFromHex(ID)
 	/*Condicion del search*/
 	condition := bson.M{
-		"_id" : objID,
+		"_id": objID,
 	}
 
 	err := col.FindOne(context, condition).Decode(&perfil)
 	perfil.Password = ""
-	if err != nil{
-		log.Println("Registro no encontrado " + err.Error() )
-		return perfil, err 
+	if err != nil {
+		log.Println("Registro no encontrado " + err.Error())
+		return perfil, err
 	}
 
 	return perfil, nil
